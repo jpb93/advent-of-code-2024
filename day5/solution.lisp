82eb3a791b8865da237d425c05ce42d90df34b96
Jay Burkhardt <squid@MacBookChad.local>
2024-12-05 17:33:13 -0500

    Add day5 part2

--- day5/solution.lisp
+++ day5/solution.lisp
@@ -26,6 +26,7 @@
 (defparameter *rules* (create-rule-table (collect-lines *page-rules-input*)))
 (defparameter *pages* (mapcar #'pages->list (collect-lines *page-input*)))
 
+;; Helpers
 (defun in-correct-position (page pages-before)
   (let ((page-rules (gethash page *rules*)))
     (null (intersection page-rules pages-before))))
@@ -33,7 +34,16 @@
 (defun get-middle-element (list)
   (nth (floor (length list) 2) list))
 
-(defun solution-1 ()
+(defun sum-middle-elements (list)
+  (apply #'+ (mapcar #'get-middle-element list)))
+
+(defun insert-at-idx (list element idx)
+  (append (subseq list 0 idx)
+    (list element)
+    (subseq list idx)))
+
+;; PART 1
+(defun get-correct-pages ()
   (let ((correct-pages '()))
     (dolist (page-update *pages*)
       (let ((correct-position t)
@@ -45,4 +55,39 @@
           (push current-page pages-before))
         (when correct-position
               (push page-update correct-pages))))
-    (apply #'+ (mapcar #'get-middle-element correct-pages))))
+    correct-pages))
+
+(defun solution-1 ()
+  (let ((correct-pages (get-correct-pages)))
+    (sum-middle-elements correct-pages)))
+
+;; PART 2
+(defun violates-rule (current-page before-page)
+  (let ((page-rules (gethash current-page *rules*)))
+    (and page-rules (member before-page page-rules))))
+
+(defun correct-page (page)
+  (let ((corrected-page '()))
+    (dolist (current-page page)
+      (let ((inserted nil))
+        (loop for pos from 0 upto (length corrected-page)
+              do (let ((pages-before (subseq corrected-page 0 pos))
+                       (pages-after (subseq corrected-page pos)))
+                   (if (and (every #'(lambda (before-page)
+                                       (in-correct-position current-page (list before-page)))
+                              pages-before)
+                            (every #'(lambda (after-page)
+                                       (in-correct-position after-page (list current-page)))
+                              pages-after))
+                       (progn
+                        (setf corrected-page
+                          (append pages-before (list current-page) pages-after))
+                        (setf inserted t)
+                        (return)))))
+        (unless inserted
+          (push current-page corrected-page))))
+    (nreverse corrected-page)))
+
+(defun solution-2 ()
+  (let ((corrected-pages (mapcar #'correct-page (set-difference *pages* (get-correct-pages)))))
+    (sum-middle-elements corrected-pages)))
